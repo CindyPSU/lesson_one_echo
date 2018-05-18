dcae6e4e2269c839a57cb022776c265c070932ca
Cindy <cindy.d.taylor@gmail.com>
2018-05-17 19:43:58 -0600

    commit for initial project setup

---
+++ LessonOneEcho_Group3/src/Client.java
@@ -0,0 +1,23 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
+/**
+ *
+ * @author IST 411 - Group 3 (Christopher Ferrino, Arlan Hamilton, Raymond Han, Cynthia Hilgeman)
+ */
+public class Client {
+    
+    Object clientSocket;
+    
+    public static void main(String[] args) 
+    {
+        System.out.println("Hello from Client");   
+    }
+}
---
+++ LessonOneEcho_Group3/src/Server.java
@@ -0,0 +1,22 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ *
+ * @author IST 411 - Group 3 (Christopher Ferrino, Arlan Hamilton, Raymond Han, Cynthia Hilgeman)
+ */
+public class Server {
+    
+    Object clientSocket;
+    
+    public static void main(String[] args) {
+        System.out.println("Hello from Server");
+        
+    }
+}
+  
